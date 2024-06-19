988c54d7da2da5f2b38b13f868ee74f28a37bd8a
Ishita Chakkalakkal <ishita.chakkalakkal@cloudgensys.com>
2024-06-19 09:50:30 -0400

    changes added

--- stream.py
+++ stream.py
@@ -2,8 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.write("Hello World")
-st.write("## This is a H2 Title!")
+st.write("Hello World!")
+st.write("## This is an H2 Title!")
 x = st.text_input("Movie", "Star Wars")
 
 if st.button("Click Me"):
